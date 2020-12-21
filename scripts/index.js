37fbbc8aacf7f5bbb017c968726a0d128615da62
VladimirKalachev <65942414+VladimirKalachev@users.noreply.github.com>
2020-12-21 11:19:44 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,6 @@ const deactivationPlayer = () => {
 };
 
 
-
 playerBtn.forEach((btn, i) =>  btn.addEventListener('click', () => {
         deactivationPlayer();
         btn.classList.add('active');
